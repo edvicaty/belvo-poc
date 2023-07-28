30d45496a68f27bc6daa5e6e4ae960a8148334fb
edvicaty <edvicaty@hotmail.com>
2023-07-28 14:06:36 -0500

    Implement UserDetails interface for User Class

--- src/main/java/com/belvopoc/belvopoc/user/User.java
+++ src/main/java/com/belvopoc/belvopoc/user/User.java
@@ -5,6 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Builder
 @NoArgsConstructor
@@ -12,7 +16,7 @@ import lombok.NoArgsConstructor;
 @Data
 @Entity
 @Table(name = "_user")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -22,4 +26,33 @@ public class User {
     private String email;
     private String password;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
