e447c47d6296fd996d130ed791a3543727c7570c
edvicaty <edvicaty@hotmail.com>
2023-07-28 13:54:17 -0500

    Add User class

---
+++ src/main/java/com/belvopoc/belvopoc/user/User.java
@@ -0,0 +1,25 @@
+package com.belvopoc.belvopoc.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+}
