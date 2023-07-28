b92337fd7c2badf6a6ff3c5ca98cfad9a9d7429d
edvicaty <edvicaty@hotmail.com>
2023-07-28 13:25:34 -0500

    initial commit

---
+++ src/test/java/com/belvopoc/belvopoc/BelvoPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.belvopoc.belvopoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelvoPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
