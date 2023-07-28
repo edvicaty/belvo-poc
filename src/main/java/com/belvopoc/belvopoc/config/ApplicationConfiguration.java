882d8514ab74f6484cacd80ba9935e9dd47fd4fe
edvicaty <edvicaty@hotmail.com>
2023-07-28 16:44:47 -0500

    Update JwtAuthenticationFilter for updating the Security Context if the token is valid

---
+++ src/main/java/com/belvopoc/belvopoc/config/ApplicationConfiguration.java
@@ -0,0 +1,26 @@
+package com.belvopoc.belvopoc.config;
+
+import com.belvopoc.belvopoc.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfiguration {
+    private final UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        // A lambda function could've been use here for simplicity
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                return userRepository.findByEmail(username);
+            }
+        };
+    }
+}
--- src/main/java/com/belvopoc/belvopoc/config/JwtAuthenticationFilter.java
+++ src/main/java/com/belvopoc/belvopoc/config/JwtAuthenticationFilter.java
@@ -1,11 +1,16 @@
 package com.belvopoc.belvopoc.config;
 
+import com.belvopoc.belvopoc.user.User;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -18,6 +23,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     // Use of jwtService for manipulating JWTs
     private final JwtService jwtService;
+    // A managed Bean is set for adding the required methods (on the ApplicationConfiguration Class)
+    private final UserDetailsService userDetailsService;
 
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
@@ -37,5 +44,29 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         // Extract the user email from JWT token
         final String userEmail = jwtService.extractUsername(jwt);
 
+        // If the UserEmail is null or the User is already authenticated, continue to the next filter
+        if (userEmail == null || SecurityContextHolder.getContext().getAuthentication() == null) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        User user = (User) this.userDetailsService.loadUserByUsername(userEmail);
+
+        // Provide an early return if the token is not valid
+        if (!jwtService.isTokenValid(jwt, user)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // If Token is valid, update security Context with a new value
+        // Create new token
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+        // Add details to the token from the request object
+        authToken.setDetails(
+                new WebAuthenticationDetailsSource().buildDetails(request)
+        );
+        // update Security Context
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+        filterChain.doFilter(request, response);
     }
 }
