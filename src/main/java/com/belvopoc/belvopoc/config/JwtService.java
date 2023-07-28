ba0be9a0d6c05cdbd34e1957f520fae66e412a9b
edvicaty <edvicaty@hotmail.com>
2023-07-28 15:51:34 -0500

    Add Generate token method in JwtService

--- src/main/java/com/belvopoc/belvopoc/config/JwtService.java
+++ src/main/java/com/belvopoc/belvopoc/config/JwtService.java
@@ -2,12 +2,16 @@ package com.belvopoc.belvopoc.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
@@ -16,6 +20,8 @@ public class JwtService {
     // Not making the variable final due to potential errors at runtime when using the @Value annotation
     @Value("${jwt.secretKey}")
     private String SECRET_KEY;
+    // Set expiration time to a day
+    private final Integer expirationTimeMillis = 1000 * 60 * 60 * 24;
 
     public String extractUsername(String jwt) {
         // We are expecting the subject (username) to be email of the User
@@ -43,6 +49,32 @@ public class JwtService {
     private Key getSigningKey() {
         byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
         return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+
+    // This method gets a map for the extra claims that we want to provide, It takes a User which implements UserDetails
+    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                // We are using the email as the Username
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expirationTimeMillis))
+                .signWith(getSigningKey(), SignatureAlgorithm.ES256)
+                .compact();
+    }
+
+    // Overloading the method without any extra claims
+    public String generateToken(UserDetails userDetails) {
+        return Jwts
+                .builder()
+                // We are using the email as the Username
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expirationTimeMillis))
+                .signWith(getSigningKey(), SignatureAlgorithm.ES256)
+                .compact();
     }
 
 }
