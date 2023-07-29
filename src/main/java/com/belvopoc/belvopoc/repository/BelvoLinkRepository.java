d3e164b75936ecb6efe80c0f47744b7410d9626a
edvicaty <edvicaty@hotmail.com>
2023-07-29 15:35:34 -0500

    Add Belvo Link and Institution repositories

---
+++ src/main/java/com/belvopoc/belvopoc/repository/BelvoLinkRepository.java
@@ -0,0 +1,7 @@
+package com.belvopoc.belvopoc.repository;
+
+import com.belvopoc.belvopoc.domain.BelvoLink;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BelvoLinkRepository extends JpaRepository<BelvoLink, Long> {
+}
---
+++ src/main/java/com/belvopoc/belvopoc/repository/InstitutionRepository.java
@@ -0,0 +1,7 @@
+package com.belvopoc.belvopoc.repository;
+
+import com.belvopoc.belvopoc.domain.Institution;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InstitutionRepository extends JpaRepository<Institution, Long> {
+}
