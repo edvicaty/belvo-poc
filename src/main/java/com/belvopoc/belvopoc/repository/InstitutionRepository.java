8210b634b30668df1d7a8e5a12ec77ed167429a8
edvicaty <edvicaty@hotmail.com>
2023-07-29 18:48:05 -0500

    Add findByName method on InstitutionRepository

--- src/main/java/com/belvopoc/belvopoc/repository/InstitutionRepository.java
+++ src/main/java/com/belvopoc/belvopoc/repository/InstitutionRepository.java
@@ -4,4 +4,7 @@ import com.belvopoc.belvopoc.domain.Institution;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InstitutionRepository extends JpaRepository<Institution, Long> {
+
+    Institution findByName(String name);
+
 }
