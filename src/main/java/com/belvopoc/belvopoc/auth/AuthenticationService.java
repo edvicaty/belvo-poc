3e519d3a5aaa5e1e3d42c9ac787987b0c98e1f1d
edvicaty <edvicaty@hotmail.com>
2023-07-28 23:33:37 -0500

    Add AuthenticationService with register and authenticate methods

---
+++ src/main/java/com/belvopoc/belvopoc/auth/AuthenticationService.java
@@ -0,0 +1,57 @@
+package com.belvopoc.belvopoc.auth;
+
+import com.belvopoc.belvopoc.config.JwtService;
+import com.belvopoc.belvopoc.user.Role;
+import com.belvopoc.belvopoc.user.User;
+import com.belvopoc.belvopoc.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    // TODO: add exception handling on register and authenticate methods
+    // Create User, add it to the database and return token
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        userRepository.save(user);
+
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+
+        // If the username and password are correct, generate token
+        var user = userRepository.findByEmail(request.getEmail());
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+
+    }
+}
