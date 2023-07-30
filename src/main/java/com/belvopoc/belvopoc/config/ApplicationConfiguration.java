be138a32a511444ccae2f1dcb1e2ccac4329f264
edvicaty <edvicaty@hotmail.com>
2023-07-29 21:11:15 -0500

    Add BelvoHttpService

--- src/main/java/com/belvopoc/belvopoc/config/ApplicationConfiguration.java
+++ src/main/java/com/belvopoc/belvopoc/config/ApplicationConfiguration.java
@@ -13,6 +13,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @RequiredArgsConstructor
@@ -48,5 +49,10 @@ public class ApplicationConfiguration {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
         return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
---
+++ src/main/java/com/belvopoc/belvopoc/service/BelvoHttpService.java
@@ -0,0 +1,37 @@
+package com.belvopoc.belvopoc.service;
+
+import com.belvopoc.belvopoc.domain.BelvoLink;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Base64;
+import java.util.Collections;
+
+
+@Service
+@RequiredArgsConstructor
+public class BelvoHttpService {
+    private final RestTemplate restTemplate;
+
+    @Value("${api.belvoApiBaseUrl}")
+    private String baseUrl;
+
+    @Value("${api.belvoApiId}")
+    private String apiId;
+
+    @Value("${api.belvoApiPassword}")
+    private String apiPassword;
+
+    private HttpHeaders getPostHeaders() {
+        String authorizationHeader = apiId + ":" + apiPassword;
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        headers.set("Authorization", "Basic " + Base64.getEncoder().encodeToString(authorizationHeader.getBytes()));
+        return headers;
+    }
+
+}
